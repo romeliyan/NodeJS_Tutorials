43783769bc0edde4d428d786bd2abaf2ed80ef2e
romeliyan <rkbliyanage@gmail.com>
2019-05-04 19:14:54 +0530

    T4 - Global Object

---
+++ app.js
@@ -0,0 +1,25 @@
+//console.log('Hello World');
+
+//setTimeout(function(){console.log('after 3');},3000);
+
+// var time = 0;
+// setInterval(function(){
+//   time += 2;
+//   console.log(time + 'passed');},2000);
+
+// var time = 0;
+//
+// var timer = setInterval(function(){
+//   time += 2;
+//   console.log(time + 'seconds have passed');
+//
+//   if(time > 5){
+//     clearInterval(timer);
+//   }
+// }, 2000);
+
+//  gives the directory name where we are  -->  __dirname
+console.log(__dirname);
+
+// gives the file name which file in we are -->  __filename
+console.log(__filename);
